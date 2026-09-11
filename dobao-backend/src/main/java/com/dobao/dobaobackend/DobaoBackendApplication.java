17c9466e11948a865aaafabb51737e8d085832ac
tangwang <bear2-tangwang@outlook.com>
2026-09-11 17:32:28 +0800

    init : 后端初始化

---
+++ dobao-backend/src/main/java/com/dobao/dobaobackend/DobaoBackendApplication.java
@@ -0,0 +1,14 @@
+package com.dobao.dobaobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DobaoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DobaoBackendApplication.class, args);
+        System.out.println("DobaoBackendApplication started");
+    }
+
+}
